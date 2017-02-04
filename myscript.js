5b29dedbed397ad8421c1f7fed61d1242fa57d26
b6112166 <959321146@qq.com>
2017-02-03 16:59:58 -0800

    with count

--- myscript.js
+++ myscript.js
@@ -1,5 +1,19 @@
+
+var  a = 1;
 function displayimg(){
 	
 	document.getElementById('img').src= 'imgres.jpg'
 
+}
+function display_time(){
+	document.getElementById('time').innerHTML= Date()
+	
+}
+
+
+function count(){
+	
+	document.getElementById('count').innerHTML= a
+	a++
+	
 }
